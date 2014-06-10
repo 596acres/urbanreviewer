a8fcf26e82de1bca26d5cd87aa5b4653b01450d5
Eric Brelsford <ebrelsford@gmail.com>
2014-06-10 16:51:11 -0400

    Add BBLs to CSVs

---
+++ data/add_bbls.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+
+
+def add_bbls(fp):
+    """
+    Add BBLs (Borough, Block, and Lot numbers) to a csv of lots with the
+    constituent fields.
+    """
+    reader = csv.DictReader(fp)
+    print 'BBL,' + ','.join(reader.fieldnames)
+    for row in reader:
+        try:
+            bbl = '%d%05d%04d' % tuple(int(row[c]) for c in ('Borough', 'Block', 'Lot'))
+            print ','.join([bbl,] + [row[f] for f in reader.fieldnames])
+        except Exception:
+            continue
+
+
+if __name__ == '__main__':
+    add_bbls(sys.stdin)
